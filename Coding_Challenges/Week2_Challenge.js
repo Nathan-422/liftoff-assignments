fca7c31aa4d94d846a2f252ecbfc2d0e6676500b
Nathan-422 <natiouswright@gmail.com>
2023-04-18 18:22:18 -0500

    Week 2 coding challenge complete

--- Coding_Challenges/Week2_Challenge.js
+++ Coding_Challenges/Week2_Challenge.js
@@ -1,7 +1,17 @@
 function minMax(arrayOfNumbers) {
+    
+    // could also check for an array in the first place
+
+    if (arrayOfNumbers.length === 0) {
+        return null;
+    }
+    
     let min = arrayOfNumbers[0];
     let max = arrayOfNumbers[0];
     for (const num of arrayOfNumbers) {
+
+        // could check that the element is a number
+
         if (num < min) {
             min = num;
         }
