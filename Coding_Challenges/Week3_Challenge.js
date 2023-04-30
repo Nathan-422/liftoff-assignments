08c9680e8c8bf0aea3707e6e4f8465dd48848382
Nathan-422 <natiouswright@gmail.com>
2023-04-29 19:51:35 -0500

    Week three challenge complete

---
+++ Coding_Challenges/Week3_Challenge.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+
+    // code goes here  
+    
+    return str.split('').reverse().join(''); 
+  
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse(readline()));
